7c2852131597a5a1e8d63199caa755a4c576ae97
森篤史 <tb.e.zizii82@gmail.com>
2016-04-13 17:30:07 +0900

    第１章 練習問題１ プログラム１

---
+++ section1/dr1_1.c
@@ -0,0 +1,25 @@
+/*
+Hornerの方法
+*/
+
+#include <stdio.h>
+
+double fn(double, double *, int);
+
+int main(void) {
+	double a[] = {1, 2, 3, 4, 5}; /* 係数 */
+	double x;
+
+	for(x = 1; x <= 5; x++)
+		printf("fn(%f)=%f\n", x, fn(x, a, 4));
+}
+
+double fn(double x, double a[], int n) {
+	double p;
+	int i;
+
+	p = a[n];
+	for(i = n - 1; i >= 0; i--)
+		p = p*x+a[i];
+	return p;
+}
