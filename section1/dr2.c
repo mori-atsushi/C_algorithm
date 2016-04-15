e09686a6b846dae90af3cbf4902cf6b8086da9cf
森篤史 <tb.e.zizii82@gmail.com>
2016-04-15 16:02:53 +0900

    1-2 写像 練習問題2 暗号

---
+++ section1/dr2.c
@@ -0,0 +1,26 @@
+/*
+暗号の解読
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	char table[] = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O',
+	                'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
+	                'L', 'Z', 'X', 'C', 'V' ,'B', 'N', 'M', '?'};
+	char *p, *ango = "KSOIDHEPZ";
+	int index;
+
+	p = ango;
+	while(*p != '\0') {
+		if('A' <= *p && *p <= 'Z')
+			index = *p - 'A';
+		else
+			index = 26;
+		putchar(table[index]);
+		p++;
+	}
+	printf("\n");
+
+	return 0;
+}
