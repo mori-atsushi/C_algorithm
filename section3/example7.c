cf06bd65543e76ed2e746b19941335425bd28776
森篤史 <tb.e.zizii82@gmail.com>
2016-05-10 22:12:06 +0900

    2-7 文字列の置き換え(リプレイス) 例題24 リプレイス

---
+++ section3/example7.c
@@ -0,0 +1,41 @@
+/*
+文字列の置き換え(リプレイス)
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+char *search(char *, char *);
+void replace(char *, char *, char *);
+
+int main(void) {
+	static char text[] = "This is a pen. That is a pencil.";
+
+	replace(text, "pen", "cap"); /* 同じ長さであること */
+	printf("%s\n", text);
+
+	return 0;
+}
+
+void replace(char *text, char *key, char *rep) {
+	char *p;
+	int i;
+	while((p = search(text, key)) != NULL) {
+		for(i = 0; i < (int)strlen(rep); i++) /* 置き換え */
+			p[i] = rep[i];
+	}
+}
+
+char *search(char *text, char *key) {
+	int m, n;
+	char *p;
+
+	m = strlen(text);
+	n = strlen(key);
+	for(p = text; p <= text + m - n; p++) {
+		if(strncmp(p, key, n) == 0)
+			return p;
+	}
+
+	return NULL;
+}
