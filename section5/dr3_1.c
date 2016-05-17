cd9b7e66efb9c428476564fd3d86c8f4fef8f5f8
森篤史 <tb.e.zizii82@gmail.com>
2016-05-17 23:29:01 +0900

    5-3 リストの作成 練習問題34-1 入力順のリストの作成

---
+++ section5/dr3_1.c
@@ -0,0 +1,40 @@
+/*
+リストデータの制作
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct temp{
+	char name[20]; /* 名前 */
+	char tel[20]; /* 電話番号 */
+	struct temp *pointer; /* 次データへのポインタ */
+} tfield;
+
+tfield *talloc(void);
+
+int main(void) {
+	tfield *head, *p, *old;
+	head = talloc();
+	scanf("%s %s", head->name, head->tel);
+	old = head;
+
+	while(p = talloc(), scanf("%s %s", p->name, p->tel) != EOF) {
+		if(!strcmp(p->name, "q"))
+			break;
+		old->pointer = p;
+		old = p;
+	}
+
+	p = head;
+	while(p != NULL) {
+		printf("%15s%15s\n", p->name, p->tel);
+		p = p->pointer;
+	}
+}
+
+/* 記憶領域の確保 */
+tfield *talloc(void) {
+	return (tfield *)malloc(sizeof(tfield));
+}
