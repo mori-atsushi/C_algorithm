3fbfdf0bc32937ba0bcf32c5d0d77292f0e2dfa0
森篤史 <tb.e.zizii82@gmail.com>
2016-05-17 23:25:44 +0900

    5-3 リストの作成 例題34 入力とは逆順なリストの作成

---
+++ section5/example3.c
@@ -0,0 +1,38 @@
+/*
+リストデータの制作
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct temp{
+	char name[20]; /* 名前 */
+	char tel[20]; /* 電話番号 */
+	struct temp *pointer; /* 次データへのポインタ */
+} tfield;
+
+tfield *talloc(void);
+
+int main(void) {
+	tfield *head, *p;
+	head = NULL;
+
+	while(p = talloc(), scanf("%s %s", p->name, p->tel) != EOF) {
+		if(!strcmp(p->name, "q"))
+			break;
+		p->pointer = head;
+		head = p;
+	}
+
+	p = head;
+	while(p != NULL) {
+		printf("%15s%15s\n", p->name, p->tel);
+		p = p->pointer;
+	}
+}
+
+/* 記憶領域の確保 */
+tfield *talloc(void) {
+	return (tfield *)malloc(sizeof(tfield));
+}
