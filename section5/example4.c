1e4d1562089004f207aef729185674adfb5ea8b6
森篤史 <tb.e.zizii82@gmail.com>
2016-05-19 01:02:53 +0900

    5-4 リストへの挿入 例題35 リストへの挿入

---
+++ section5/example4.c
@@ -0,0 +1,79 @@
+/*
+リストへの追加
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct temp{
+	char name[20]; /* 名前 */
+	char tel[20]; /* 電話番号 */
+	struct temp *pointer; /* 次データへのポインタ */
+} tfield;
+
+tfield *head;
+tfield *talloc(void);
+void genlist(void);
+void displist(void);
+void link(char *);
+
+int main(void) {
+	char key[20];
+
+	genlist();
+	displist();
+
+	while(printf("Key Name : "), scanf("%s", key) != EOF) {
+		link(key);
+		displist();
+	}
+}
+
+/* リストへの追加 */
+void link(char *key) {
+	tfield *p, *n;
+
+	n = talloc();
+	printf("追加データ : ");
+	scanf("%s %s", n->name, n->tel);
+	p = head;
+	while(p != NULL) {
+		if(strcmp(key, p->name) == 0) {
+			n->pointer = p->pointer;
+			p->pointer = n;
+			return ;
+		}
+		p = p->pointer;
+	}	
+	printf("キーデータが見つかりません\n");
+}
+
+/* リストの作成 */
+void genlist(void) {
+	tfield *p;
+
+	head = NULL;
+	while(p = talloc(), scanf("%s %s", p->name, p->tel) != EOF) {
+		if(!strcmp(p->name, "q"))
+			break;
+		p->pointer = head;
+		head = p;
+	}
+}
+
+/* リストの表示 */
+void displist(void) {
+	tfield *p;
+	p = head;
+
+	while(p != NULL) {
+		printf("%15s%15s\n", p->name, p->tel);
+		p = p->pointer;
+	}
+}
+
+/* 記憶領域の確保 */
+tfield *talloc(void) {
+	return (tfield *)malloc(sizeof(tfield));
+}
