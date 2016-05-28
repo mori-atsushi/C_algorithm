9cd5687a6ea9818183953a8d85994ba873b1cf7d
森篤史 <tb.e.zizii82@gmail.com>
2016-05-28 12:35:57 +0900

    6-3 2分探索木の再帰的表現 例題44 2分探索木の作成(再帰版)

---
+++ section6/example3.c
@@ -0,0 +1,47 @@
+/*
+2分探索木の作成(再帰版)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct temp{
+	struct temp *left;
+	char name[12];
+	struct temp *right;
+} tnode;
+
+tnode *talloc(void);
+tnode *gentree(tnode * , char *);
+
+int main(void) {
+	char dat[12];
+	tnode *root;
+
+	root = NULL;
+	while(scanf("%s", dat) != EOF)
+		root = gentree(root, dat);
+
+	return 0;
+}
+
+/* 木の作成の再帰手続き */
+tnode *gentree(tnode *p, char *w) {
+	if(p == NULL) {
+		p = talloc();
+		strcpy(p->name, w);
+		p->left = p->right = NULL;
+	} else if(strcmp(w, p->name) < 0) {
+		p->left = gentree(p->left, w);
+	} else {
+		p->right = gentree(p->right, w);
+	}
+
+	return p;
+}
+
+/* 記憶領域の取得 */
+tnode *talloc(void) {
+	return (tnode *)malloc(sizeof(tnode));
+}
