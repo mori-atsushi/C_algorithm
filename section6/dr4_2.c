360b00ea709946100352bb746a80424f0cd41d08
森篤史 <tb.e.zizii82@gmail.com>
2016-05-29 21:58:39 +0900

    6-4 2分探索木のトラバーサル 練習問題45-2 トラバーサルの非再帰版

---
+++ section6/dr4_2.c
@@ -0,0 +1,69 @@
+/*
+2分探索木のトラバサール(非再帰版)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct temp{
+	struct temp *left;
+	char name[12];
+	struct temp *right;
+} tnode;
+
+tnode *talloc(void);
+tnode *gentree(tnode * , char *);
+void treewalk(tnode *);
+
+int main(void) {
+	char dat[12];
+	tnode *root;
+
+	root = NULL;
+	while(scanf("%s", dat) != EOF) {
+		if(strcmp("q", dat) == 0)
+			break;
+		root = gentree(root, dat);
+	}
+	treewalk(root);
+
+	return 0;
+}
+
+/* 木のトラバサール(非再帰版) */
+void treewalk(tnode *p) {
+	tnode *q, *w[128];
+	int sp = 0;
+
+	q = p;
+	while(!(sp == 0 && q == NULL)) {
+		while(q != NULL) {
+			w[sp++] = q;
+			q = q->left;
+		}
+		sp--;
+		printf("%s\n", w[sp]->name);
+		q = w[sp]->right;
+	}
+}
+
+/* 木の作成の再帰手続き */
+tnode *gentree(tnode *p, char *w) {
+	if(p == NULL) {
+		p = talloc();
+		strcpy(p->name, w);
+		p->left = p->right = NULL;
+	} else if(strcmp(w, p->name) < 0) {
+		p->left = gentree(p->left, w);
+	} else {
+		p->right = gentree(p->right, w);
+	}
+
+	return p;
+}
+
+/* 記憶領域の取得 */
+tnode *talloc(void) {
+	return (tnode *)malloc(sizeof(tnode));
+}
