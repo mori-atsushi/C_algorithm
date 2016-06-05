f5bc4d5a607a8f77ca2bf0414768250f0b604d3d
森篤史 <tb.e.zizii82@gmail.com>
2016-06-05 23:26:30 +0900

    7-5 最短経路問題 例題55 ダイクストラ法

---
+++ section7/example5.c
@@ -0,0 +1,57 @@
+/*
+最短経路問題(ダイクストラ法)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 8
+#define M 9999
+
+int a[N + 1][N + 1] = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                       {0, 0, 1, 7, 2, M, M, M, M},
+                       {0, 1, 0, M, M, 2, 4, M, M},
+                       {0, 7, M, 0, M, M, 2, 3, M},
+                       {0, 2, M, M, 0, M, M, 5, M},
+                       {0, M, 2, M, M, 0, 1, M, M},
+                       {0, M, 4, 2, M, 1, 0, M, 6},
+                       {0, M, M, 3, 5, M, M, 0, 2},
+                       {0, M, M, M, M, M, 6, 2, 0}};
+
+int main(void) {
+	int j, k, p, start, min, leng[N + 1], v[N + 1];
+
+	printf("始点 ");
+	scanf("%d", &start);
+	for(k = 1; k <= N; k++) {
+		leng[k] = M;
+		v[k] = 0;
+	}
+	
+	leng[start] = 0;
+
+	for(j = 1; j <= N; j++) {
+		min = M;
+		for(k = 1; k <= N; k++) {
+			if(v[k] == 0 && leng[k] < min) {
+				p = k;
+				min = leng[k];
+			}
+		}
+		v[p] = 1;
+
+		if(min == M) {
+			printf("グラフは連結でない\n");
+			exit(1);
+		}
+
+		/* pを経由してkに至る長さがそれまでの最短路より小さければ更新 */
+		for(k = 1; k <= N; k++) {
+			if((leng[p] + a[p][k]) < leng[k])
+				leng[k] = leng[p] + a[p][k];
+		}
+	}
+
+	for(j = 1; j <= N; j++)
+		printf("%d -> %d : %d\n", start, j, leng[j]);
+}
